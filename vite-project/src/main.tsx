288775032878aab69fff7144edebb573f418f16d
mahsannawaz1 <nawazehsen@gmail.com>
2024-03-17 18:43:58 +0500

    Change Font family for the project to Montserrat

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import theme from "./themes/theme.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
